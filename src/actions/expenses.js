bbb18adeded8f420fe73dccd732756d551aba015
Mac <david nwachukwu>
2022-11-13 19:21:01 +0100

    private Routes

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -39,12 +39,21 @@ export const startRemoveExpense = ({id} = {} )=>{
       });
     };
 };
+
 //edit_expense
 export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
     id,
     updates
-});        
+});    
+
+export const startEditExpense = (id, updates) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).update(updates).then(()=> {
+            dispatch(editExpense(id, updates));
+        });
+    };
+};
 
 //set expenses
 
